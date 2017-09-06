71a4d89a6f2d4104e46e5a2ff922dbfdb2e56a77
Chris Johnson <chrisjohnson112@yahoo.com>
2017-09-06 05:09:02 -0500

    initial upload

---
+++ contact.php
@@ -0,0 +1,60 @@
+<?php 
+
+session_start();
+
+require_once 'phpmailer/PHPMailerAutoload.php';
+
+$errors = [];
+
+
+if(isset($_POST['name'], $_POST['email'], $_POST['comment'])) {
+	$fields = [
+		'name' => $_POST['name'],
+		'email' => $_POST['email'],
+		'comment' => $_POST['comment']		
+	];
+
+	foreach($fields as $field => $data) {
+		if(empty($data)) {
+			$errors[] = 'The '. field . ' field is required.';
+		}
+	}
+
+	if(empty($errors)) {
+		$m = new PHPMailer;
+
+		$m->isSMTP();
+		$m->SMTPAuth=true;
+		//use your SMTP details here
+
+		$m->SMTPDebug = 1;
+		$m ->Host = "a2plcpnl0866.prod.iad2.secureserver.net";
+		$m->Username = 'chris_johnson@substyleprod.com';
+		$m->Password = '12KeND56Ra';
+		$m->SMTPSecure = 'ssl';
+		$m ->IsHTML(true);
+		$m->Port = 465;
+
+		$m->Subject = 'Contact form submitted';
+		$m->Body = 'From: ' . $fields['name'] . ' (' . $fields['email']. ')<p>'. $fields['comment'].'</p>';
+
+		$m->FromName = 'Contact';
+
+		$m->AddAddress('chris_johnson@substyleprod.com', 'Chris Johnson');
+
+		if($m->send()) {
+			header('Location: thanks.php');
+			die();
+		} else {
+			$errors[] = 'Sorry, could not send email. Try again later.';
+		}
+		
+	}
+
+} else {
+	$errors[] ='Something went wrong.';
+}
+
+
+
+header('Location: index.php');
---
+++ thanks.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Thanks!</title>
+</head>
+<link rel="Stylesheet" href="style.css">
+<body>Thanks, we have your message and will be in touch with you soon!
+</body>
+</html>
